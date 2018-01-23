54c9f96e13bbde2262fef15dabdde9a7c4f06614
Ashish <ashishkadasi99@gmail.com>
2018-01-23 17:56:42 -0600

    Fibonacci

---
+++ TestCode/src/com/test/code/Fibonacci.java
@@ -0,0 +1,23 @@
+package com.test.code;
+import java.lang.*;
+import java.io.*;
+
+
+public class Fibonacci {
+
+	public static void main(String[] args) {
+		int num1=0, num2=1, num3,j;
+		int count=10;
+		System.out.println(num1+"\n"+""+num2);
+		
+		for (int i = 2; i < count; ++i) {
+			num3=num1+num2;
+			System.out.println(""+num3);
+			num1=num2;
+			num2=num3;
+			
+		}
+
+	}
+
+}
